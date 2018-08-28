1ba38d0003555b1fdd6def47ab7ad6df563c50fc
Bartłomiej Górnicki <bartlomiej@gornicki.org>
2018-08-28 22:32:44 +0200

    Changed way notification are periodically checked

--- scripts/background.js
+++ scripts/background.js
@@ -15,6 +15,8 @@ var alwaysShowMainPage = false;
 var shouldCountHashtags = false;
 var interval = 10;
 
+let intervalHandler = null;
+
 var COLOR_NEW_NOTIFICATION = {
 	color: [204, 0, 51, 255]
 };
@@ -47,7 +49,8 @@ function init() {
 			chrome.browserAction.onClicked.addListener(iconClickHandler);
 		}
 
-		setTimeout(getNotifications, 1000);
+		clearTimeout(intervalHandler);
+		intervalHandler = setTimeout(getNotifications, 1000);
 	});
 }
 
@@ -89,100 +92,94 @@ function getUserSettings(callback) {
 
 
 function isNotificationFromTag(notificationObject) {
-	return ( (notificationObject.outerHTML.indexOf("użył tagu") > 0) || (notificationObject.outerHTML.indexOf("użyła tagu") > 0) );
+	return ((notificationObject.outerHTML.indexOf("użył tagu") > 0) || (notificationObject.outerHTML.indexOf("użyła tagu") > 0));
 }
 
 function getNotifications() {
-	var req = new XMLHttpRequest();
+
+	let req = new XMLHttpRequest();
 	req.open('GET', 'https://www.wykop.pl/powiadomienia/', true);
 	req.timeout = 500; // time in milliseconds 
-	req.onerror = function () {
-		console.log("Error occured. Rescheduling");
-		setTimeout(getNotifications, checkingInterval_ms);
-	};
 	req.onreadystatechange = function (aEvt) {
-		if (req.readyState == 4) {
-			if (req.status == 200) {
+		if (req.readyState == 4 && req.status == 200) {
+			if (isUserLoggedIn(req.responseText)) {
 
-				if (isUserLoggedIn(req.responseText)) {
+				totalNotificationCount = getMentionsCount(req.responseText);
+				if (shouldCountHashtags) {
+					totalNotificationCount += getHashtagsCount(req.responseText);
+				}
 
-					totalNotificationCount = getMentionsCount(req.responseText);
-					if (shouldCountHashtags) {
-						totalNotificationCount += getHashtagsCount(req.responseText);
-					}
-
-					if (totalNotificationCount > totalNotificationCountOld) {
-						if (soundsEnabled) {
-							if (soundsOnlyFirst == false) {
+				if (totalNotificationCount > totalNotificationCountOld) {
+					if (soundsEnabled) {
+						if (soundsOnlyFirst == false) {
+							audio.play();
+						} else {
+							if (totalNotificationCountOld == 0) {
 								audio.play();
-							} else {
-								if (totalNotificationCountOld == 0) {
-									audio.play();
-								}
 							}
-							totalNotificationCountOld = totalNotificationCount;
 						}
-					} else {
 						totalNotificationCountOld = totalNotificationCount;
 					}
+				} else {
+					totalNotificationCountOld = totalNotificationCount;
+				}
 
-					if (totalNotificationCount != 0) {
-						notificationCountString = totalNotificationCount.toString();
-					} else {
-						notificationCountString = "";
-					}
+				if (totalNotificationCount != 0) {
+					notificationCountString = totalNotificationCount.toString();
+				} else {
+					notificationCountString = "";
+				}
 
-					extensionSetBadge(COLOR_NEW_NOTIFICATION, notificationCountString);
-					extensionSetTitle("Wykop - zalogowany: \n " + getMentionsCount(req.responseText) + " powiadomien\n " + getHashtagsCount(req.responseText) + " tagow");
+				extensionSetBadge(COLOR_NEW_NOTIFICATION, notificationCountString);
+				extensionSetTitle("Wykop - zalogowany: \n " + getMentionsCount(req.responseText) + " powiadomien\n " + getHashtagsCount(req.responseText) + " tagow");
 
-					if(totalNotificationCount != 0) {
-						var parser = new DOMParser();
-						var responseDoc = parser.parseFromString(req.responseText, "text/html");
-						var notificationList = responseDoc.getElementsByClassName("menu-list notification");
-						
-						if(notificationList.length > 0) {
-							var notifications = notificationList[0].getElementsByTagName("li");
-							for (var i = 0; i < notifications.length; ++i) {
+				if (totalNotificationCount != 0) {
+					var parser = new DOMParser();
+					var responseDoc = parser.parseFromString(req.responseText, "text/html");
+					var notificationList = responseDoc.getElementsByClassName("menu-list notification");
 
-								if ( (notifications[i].classList.contains('type-light-warning')) && (!isNotificationFromTag(notifications[i])) ) {
-									collapse(notifications[i]);
-									var links = notifications[i].getElementsByTagName("a");
-									if (links.length > 0) {
-										notificationAddress = links[links.length-1].href;
-									}									
-									
-									if (showRichNotifications) {
-										if (!wasRichNotificationShown(notificationAddress)) {
-											showRichNotification(notifications[i].innerText.trim(), notificationAddress);
-										}
-									}
-									
+					if (notificationList.length > 0) {
+						var notifications = notificationList[0].getElementsByTagName("li");
+						for (var i = 0; i < notifications.length; ++i) {
+
+							if ((notifications[i].classList.contains('type-light-warning')) && (!isNotificationFromTag(notifications[i]))) {
+								collapse(notifications[i]);
+								var links = notifications[i].getElementsByTagName("a");
+								if (links.length > 0) {
+									notificationAddress = links[links.length - 1].href;
 								}
+
+								if (showRichNotifications) {
+									if (!wasRichNotificationShown(notificationAddress)) {
+										showRichNotification(notifications[i].innerText.trim(), notificationAddress);
+									}
+								}
+
 							}
-						}				
-					} else {
-						notificationsShown = [];
+						}
 					}
 				} else {
-					extensionShowError("?");
+					notificationsShown = [];
 				}
-				setTimeout(getNotifications, checkingInterval_ms);
 			} else {
-				setTimeout(getNotifications, checkingInterval_ms);
+				extensionShowError("?");
 			}
 		}
 	};
 	req.send(null);
+
+	clearTimeout(intervalHandler);
+	intervalHandler = setTimeout(getNotifications, checkingInterval_ms);
 }
 
 function iconClickHandler(tab) {
 	iconClickReport();
 	if (alwaysShowMainPage) {
 		openNewChromeTab("https://www.wykop.pl");
-	} else if(totalNotificationCount == 0) {
+	} else if (totalNotificationCount == 0) {
 		openNewChromeTab("https://www.wykop.pl");
 	} else if (totalNotificationCount == 1) {
-		if ( (notificationAddress != null) && (notificationAddress != "") ) {
+		if ((notificationAddress != null) && (notificationAddress != "")) {
 			openNewChromeTab(notificationAddress);
 		} else {
 			openNewChromeTab("https://www.wykop.pl");
@@ -225,14 +222,14 @@ function getMentionsCount(response) {
 	var bellPattern = '(id="pmNotificationsCount">)(\\d*)(</b>)';
 	var mentionsQuery = new RegExp(mentionsPattern, ["i"]);
 	var bellQuery = new RegExp(bellPattern, ["i"]);
-	
+
 	var mentionsResults = mentionsQuery.exec(response);
 	var bellResults = bellQuery.exec(response);
 	if (mentionsResults) {
 		counter += parseInt(mentionsResults[2]);
 	}
 	if (bellResults) {
-			counter += parseInt(bellResults[2]);
+		counter += parseInt(bellResults[2]);
 	}
 	return counter;
 }
@@ -268,7 +265,7 @@ function iconClickReport() {
 }
 
 function showRichNotification(text, link) {
-	
+
 	notificationsShown.push(link);
 
 	var options = {
@@ -292,10 +289,10 @@ function showRichNotification(text, link) {
 
 function wasRichNotificationShown(link) {
 	var wasShown = false;
-	
+
 	for (let index = 0; index < notificationsShown.length; index++) {
 		const element = notificationsShown[index];
-		if(element == link) {
+		if (element == link) {
 			wasShown = true;
 		}
 	}
@@ -306,7 +303,7 @@ function wasRichNotificationShown(link) {
 function deleteNotificationInfo(link) {
 	for (let i = 0; i < notificationsShown.length; i++) {
 		const element = notificationsShown[i];
-		if(element == link) {
+		if (element == link) {
 			notificationsShown.splice(i, 1);
 		}
 	}
